122f8871e6d00512250a62ffe365e98cb72ddac2
Robert Juhasz <robert.juhasz@gmail.com>
2021-08-31 22:13:30 -0500

    preprocessing for Deberta

--- cuad_model.py
+++ cuad_model.py
@@ -1,13 +1,51 @@
-from transformers import DebertaV2Tokenizer, DebertaV2Model
+from transformers import DebertaV2Tokenizer, DebertaV2ForQuestionAnswering
 import torch
 
 tokenizer = DebertaV2Tokenizer.from_pretrained('./models/deberta-v2-xlarge')
-model = DebertaV2Model.from_pretrained('./models/deberta-v2-xlarge')
+model = DebertaV2ForQuestionAnswering.from_pretrained('./models/deberta-v2-xlarge')
 
-inputs = tokenizer('Hello, my dog is cute', return_tensors='pt')
+'''
+inputs = tokenizer('What animal do I have?','Hello, my dog is cute', add_special_tokens=True, return_tensors='pt')
 outputs = model(**inputs)
 
-last_hidden_states = outputs.last_hidden_state
+input_ids = inputs["input_ids"].tolist()[0]
 
-print(outputs)
-print(last_hidden_states)
+outputs = model(**inputs)
+
+answer_start_scores = outputs[0]
+answer_end_scores = outputs[1]
+# Get the most likely beginning of answer with the argmax of the score
+answer_start = torch.argmax(answer_start_scores)
+# Get the most likely end of answer with the argmax of the score
+answer_end = torch.argmax(answer_end_scores) + 1
+
+answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+print(inputs)
+'''
+
+text = r"""
+Transformers (formerly known as pytorch-transformers and pytorch-pretrained-bert) provides general-purpose
+architectures (BERT, GPT-2, RoBERTa, XLM, DistilBert, XLNet…) for Natural Language Understanding (NLU) and Natural
+Language Generation (NLG) with over 32+ pretrained models in 100+ languages and deep interoperability between
+TensorFlow 2.0 and PyTorch.
+"""
+
+questions = [
+    "How many pretrained models are available in Transformers?",
+    "What does Transformers provide?",
+    "Transformers provides interoperability between which frameworks?",
+]
+
+for question in questions:
+    inputs = tokenizer(question, text, add_special_tokens=True, return_tensors="pt")
+    input_ids = inputs["input_ids"].tolist()[0]
+    outputs = model(**inputs)
+    answer_start_scores = outputs.start_logits
+    answer_end_scores = outputs.end_logits
+    # Get the most likely beginning of answer with the argmax of the score
+    answer_start = torch.argmax(answer_start_scores)
+    # Get the most likely end of answer with the argmax of the score
+    answer_end = torch.argmax(answer_end_scores) + 1
+    answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+    print(f"Question: {question}")
+    print(f"Answer: {answer}")
