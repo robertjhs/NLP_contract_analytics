2d7db59ab5842a906cc89930919a19c753c3071c
Robert Juhasz <robert.juhasz@gmail.com>
2021-09-01 22:08:55 -0500

    predict on single question

--- cuad_model.py
+++ cuad_model.py
@@ -1,43 +1,80 @@
+import pandas as pd
+import re
 from transformers import DebertaV2Tokenizer, DebertaV2ForQuestionAnswering
 import torch
 
 tokenizer = DebertaV2Tokenizer.from_pretrained('./models/deberta-v2-xlarge')
 model = DebertaV2ForQuestionAnswering.from_pretrained('./models/deberta-v2-xlarge')
 
+filepath = 'local_data/unlabeled_contracts/2020/000019119.txt'
+with open(filepath, 'r', encoding="utf-8") as file:
+    contract = file.read().replace('\n', '')
+
+# Clean up and pre-process the text.
+def pre_process_text(text):
+    # Simple replacement for "\n"
+    text = text.replace("\n", " ")     
+    
+    # Simple replacement for "\xa0"
+    text = text.replace("\xa0", " ")  
+    
+    # Simple replacement for "\x0c"
+    text = text.replace("\x0c", " ")
+    
+    # Get rid of multiple dots
+    regex = "\ \.\ "
+    subst = "."
+    text = re.sub(regex, subst, text, 0)
+    
+    # Get rid of underscores
+    regex = "_"
+    subst = " "
+    text = re.sub(regex, subst, text, 0)
+    
+    # Get rid of multiple dashes
+    regex = "--+"
+    subst = " "
+    text = re.sub(regex, subst, text, 0)
+    
+    # Get rid of multiple stars
+    regex = "\*+"
+    subst = "*"
+    text = re.sub(regex, subst, text, 0)
+    
+    # Get rid of multiple whitespace
+    regex = "\ +"
+    subst = " "
+    text = re.sub(regex, subst, text, 0)
+    
+    #Strip leading and trailing whitespace
+    text = text.strip()
+    
+    return text
+
+text = pre_process_text(contract)
+
+def get_questions_from_csv():
+    df = pd.read_csv("./data/category_descriptions.csv")
+    q_dict = {}
+    for i in range(df.shape[0]):
+        category = df.iloc[i, 0].split("Category: ")[1]
+        description = df.iloc[i, 1].split("Description: ")[1]
+        q_dict[category.title()] = description
+    return q_dict
+
+qtype_dict = get_questions_from_csv()
+labels = [l for l in qtype_dict.keys()]
+questions = [q for q in qtype_dict.values()]
+
+answers = []
+
+n = 2
+question = questions[n]
+label = labels[n]
+
 '''
-inputs = tokenizer('What animal do I have?','Hello, my dog is cute', add_special_tokens=True, return_tensors='pt')
-outputs = model(**inputs)
-
-input_ids = inputs["input_ids"].tolist()[0]
-
-outputs = model(**inputs)
-
-answer_start_scores = outputs[0]
-answer_end_scores = outputs[1]
-# Get the most likely beginning of answer with the argmax of the score
-answer_start = torch.argmax(answer_start_scores)
-# Get the most likely end of answer with the argmax of the score
-answer_end = torch.argmax(answer_end_scores) + 1
-
-answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-print(inputs)
-'''
-
-text = r"""
-Transformers (formerly known as pytorch-transformers and pytorch-pretrained-bert) provides general-purpose
-architectures (BERT, GPT-2, RoBERTa, XLM, DistilBert, XLNet…) for Natural Language Understanding (NLU) and Natural
-Language Generation (NLG) with over 32+ pretrained models in 100+ languages and deep interoperability between
-TensorFlow 2.0 and PyTorch.
-"""
-
-questions = [
-    "How many pretrained models are available in Transformers?",
-    "What does Transformers provide?",
-    "Transformers provides interoperability between which frameworks?",
-]
-
 for question in questions:
-    inputs = tokenizer(question, text, add_special_tokens=True, return_tensors="pt")
+    inputs = tokenizer(question, text, padding='max_length', truncation='only_second', return_tensors='pt')
     input_ids = inputs["input_ids"].tolist()[0]
     outputs = model(**inputs)
     answer_start_scores = outputs.start_logits
@@ -47,5 +84,21 @@ for question in questions:
     # Get the most likely end of answer with the argmax of the score
     answer_end = torch.argmax(answer_end_scores) + 1
     answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-    print(f"Question: {question}")
-    print(f"Answer: {answer}")
+    answers.append(answer)
+
+for l, a in zip(labels, answers):
+    print(f'{l}: {a}')
+'''
+
+inputs = tokenizer(question, text, padding='max_length', truncation='only_second', return_tensors='pt')
+input_ids = inputs["input_ids"].tolist()[0]
+outputs = model(**inputs)
+answer_start_scores = outputs.start_logits
+answer_end_scores = outputs.end_logits
+# Get the most likely beginning of answer with the argmax of the score
+answer_start = torch.argmax(answer_start_scores)
+# Get the most likely end of answer with the argmax of the score
+answer_end = torch.argmax(answer_end_scores) + 1
+answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+
+print(f'{label}: {answer}')
